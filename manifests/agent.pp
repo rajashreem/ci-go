31e810827d418d9d2f74e3b8154a6be483ec38ca
Rajashree <rajashreermalvade@gmail.com>
2014-05-17 17:47:09 +0530

    Picking up agent version, build and go_server_ip from hiera instead of accepting it as parameters

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,4 +1,9 @@
-class ci-go::agent($version = '14.1.0', $build = '18882', $go_server_ip = '127.0.0.1'){
+class ci-go::agent{
+
+  $go_config = hiera_hash('ci_go')
+  $version = $go_config['version']
+  $build = $go_config['build']
+  $go_server_ip = $go_config['server_ip']
 
   $go_agent_deb_package = "go-agent-${version}-${build}.deb"
   $go_agent_deb_package_url = "http://download01.thoughtworks.com/go/${version}/ga/${go_agent_deb_package}"
